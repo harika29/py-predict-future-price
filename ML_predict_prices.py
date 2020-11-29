a7737e50e2575bb77be14036259f319c0d9cb9b6
Harika Bodepudi <hbodepudi@intralinks.com>
2020-11-29 10:32:56 +0530

    Adding descriptive comments

--- ML_predict_prices.py
+++ ML_predict_prices.py
@@ -3,10 +3,13 @@ import matplotlib.pyplot as plot
 from sklearn.linear_model import LinearRegression
 data = pandas.read_csv('iphone_price.csv')
 print(data)
+# code to show data in scatter chart
 plot.scatter(data['version'], data['price'])
 plot.show()
+# code to show data in bar graph
 plot.bar(data['version'], data['price'])
 plot.show()
+# code to predict future data
 model = LinearRegression()
 model.fit(data[['version']], data[['price']])
 print(model.predict([[12]]))
